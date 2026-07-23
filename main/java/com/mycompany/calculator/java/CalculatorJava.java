dc890cdac42a12d645b05b4098a31bf7d26d2115
Karabo Freshly <karaborakabe92@gmail.com>
2026-07-23 18:26:19 +0200

    First commit

---
+++ main/java/com/mycompany/calculator/java/CalculatorJava.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator.java;
+
+/**
+ *
+ * @author karab
+ */
+public class CalculatorJava {
+
+    public static void main(String[] args) {
+    
+   
+     int k= 5;
+     int r= 6;
+     
+    
+     int sum = k+r;
+     int mult = k*r;
+     int sub = k-r;
+     
+     double num =5;
+     double num2 =6;
+     
+     double div = num/num2;
+            
+     System.out.println(sum);
+     System.out.println(mult);
+     System.out.println(sub);
+     System.out.println(div);
+     
+     
+     
+    
+    
+    }
+}
